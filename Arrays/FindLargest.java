c65478c88294272cd21c0c5d5acb4f7a530642ef
Shinde Samarth <166927411+SamarthShinde24@users.noreply.github.com>
2026-08-22 21:26:24 +0530

    Add largest element array problem

---
+++ Arrays/FindLargest.java
@@ -0,0 +1,16 @@
+public class FindLargest {
+    public static void main(String[] args) {
+
+        int[] arr = {3, 3, 6, 1};
+
+        int largest = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > largest) {
+                largest = arr[i];
+            }
+        }
+
+        System.out.println("Largest element: " + largest);
+    }
+}
